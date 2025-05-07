b29c7275d1dd57ac523a89d7929eb59485fd0a9c
KemAshTzy <arcayena.kemuelrollin@cec.edu.ph>
2025-05-07 22:03:11 +0800

    First Commit

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-        return "Hello, Flask!"
+    return 'Hello, Flask on Railway!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
